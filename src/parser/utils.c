3588d4018c510e8854ab5f605263f10ffb4b4330
chigvintseva <al.chigvintseva@gmail.com>
2026-03-07 14:26:22 +0100

    utils.c are norminetted and fixed

--- src/parser/utils.c
+++ src/parser/utils.c
@@ -14,6 +14,8 @@
 
 t_token	*get_last_token(t_token *head)
 {
+	if (head == NULL)
+		return (NULL);
 	while (head->next != NULL)
 		head = head->next;
 	return (head);
@@ -21,15 +23,30 @@ t_token	*get_last_token(t_token *head)
 
 int	is_redir_token(t_token_type type)
 {
-
+	if (type == REDIR_IN || type == REDIR_OUT
+		|| type == HEREDOC || type == REDIR_APPEND)
+		return (1);
+	return (0);
 }
 
 int	is_word_token(t_token_type type)
 {
-
+	if (type == WORD)
+		return (1);
+	return (0);
 }
 
-int		token_in_range(t_token *start, t_token *end, t_token *target)
+int	token_in_range(t_token *start, t_token *end, t_token *target)
 {
-
+	if (start == NULL || target == NULL || end == NULL)
+		return (0);
+	while (start != NULL && start != end)
+	{
+		if (start == target)
+			return (1);
+		start = start->next;
+	}
+	if (start == target)
+		return (1);
+	return (0);
 }
