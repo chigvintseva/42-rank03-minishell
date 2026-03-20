7d295349360334e14e2982e958d63fdd18bcc223
chigvintseva <al.chigvintseva@gmail.com>
2026-03-20 21:24:15 +0100

    cmd argv added redirs check

--- src/parser/cmd_argv.c
+++ src/parser/cmd_argv.c
@@ -6,45 +6,50 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 13:06:25 by achigvin          #+#    #+#             */
-/*   Updated: 2026/03/20 21:01:04 by achigvin         ###   ########.fr       */
+/*   Updated: 2026/03/20 21:24:01 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	add_or_free_argv(char **argv, int i, t_token *c)
+static int	redir_issue(t_token *cur)
 {
-	argv[i] = ft_strdup(c->value);
-	if (!argv[i])
-		return (-1);
-	return (i + 1);
+	cur = cur->next->next;
+	while (cur != NULL)
+	{
+		if (is_redir_token(cur))
+			cur = cur->next->next;
+		else 
+			return (1);
+	}
+	return (0);
 }
 
-char	**extract_argv(t_token *start, t_token *end, int argc)
+char	**extract_argv(t_token *cur, t_token *end, int argc)
 {
 	char	**argv;
-	t_token	*c;
 	int		i;
-	int		temp;
 
 	argv = malloc(sizeof(char *) * (argc + 1));
 	if (!argv)
 		return (NULL);
-	c = start;
 	i = 0;
-	while (c)
+	while (cur)
 	{
-		if (is_redir_token(c->type))
-			c = c->next->next;
-		else if (c->type == WORD && (temp = add_or_free_argv(argv, i, c)) < 0)
-			return (free_part_argv(argv, i), NULL);
-		else if (c->type == WORD)
-			i = temp;
-		if (c == end)
-			return (argv[i] = NULL, argv);
-		if (c == WORD)
-			return (free_part_argv(argv, i), NULL);
-		c = c->next;
+		if (is_word_token(cur->type))
+		{
+			argv[i] = ft_strdup(cur->value);
+			if (!argv[i])
+				return (free_part_argv(argv, i), NULL);
+			i++;
+			cur = cur->next;
+		}
+		else if (is_redir_token(cur->type))
+		{
+			if (redir_issue(cur))
+				return (free_part_argv(argv, i), NULL);
+			break ;
+		}
 	}
 	return (argv[i] = NULL, argv);
 }
