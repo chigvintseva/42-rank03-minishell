638f238e91e40703c3811e2c60c0e56ce944b365
Aleksandra <sergina.al.al@gmail.com>
2026-03-21 22:12:34 +0100

    working but doesnt create for argv when we dont have redirs - alena needs to fix. redirs execution dies - sasha to fix

--- src/parser/cmd_argv.c
+++ src/parser/cmd_argv.c
@@ -3,47 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_argv.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aleksandra <aleksandra@student.42.fr>      +#+  +:+       +#+        */
+/*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 13:06:25 by achigvin          #+#    #+#             */
-/*   Updated: 2026/03/21 20:02:50 by aleksandra       ###   ########.fr       */
+/*   Updated: 2026/03/21 21:53:42 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	add_or_free_argv(char **argv, int i, t_token *c)
+static int	redir_issue(t_token *cur, t_token *end)
 {
-	argv[i] = ft_strdup(c->value);
-	if (!argv[i])
-		return (-1);
-	return (i + 1);
+	if (end == NULL || cur == NULL)
+		return (0);
+	cur = cur->next;
+	while (cur != end)
+	{
+		cur = cur->next;
+		if (is_redir_token(cur->type))
+			cur = cur->next;
+		else
+			return (1);
+	}
+	return (0);
 }
 
-char	**extract_argv(t_token *start, t_token *end, int argc)
+char	**extract_argv(t_token *cur, t_token *end, int argc)
 {
 	char	**argv;
-	t_token	*c;
 	int		i;
-	int		temp;
+
 	argv = malloc(sizeof(char *) * (argc + 1));
 	if (!argv)
 		return (NULL);
-	c = start;
 	i = 0;
-	while (c)
+	while (cur != end)
 	{
-		if (is_redir_token(c->type) && (c == end || !c->next))
-			return (free_part_argv(argv, i), NULL);
-		if (is_redir_token(c->type))
-			c = c->next;
-		else if (c->type == WORD && (temp = add_or_free_argv(argv, i, c)) < 0)
-			return (free_part_argv(argv, i), NULL);
-		else if (c->type == WORD)
-			i = temp;
-		if (c == end)
-			return (argv[i] = NULL, argv);
-		c = c->next;
+		if (is_word_token(cur->type))
+		{
+			argv[i] = ft_strdup(cur->value);
+			if (!argv[i])
+				return (free_part_argv(argv, i), NULL);
+			i++;
+			cur = cur->next;
+		}
+		else if (is_redir_token(cur->type))
+		{
+			if (redir_issue(cur, end))
+				return (free_part_argv(argv, i), NULL);
+			break ;
+		}
 	}
-	return (argv);
+	return (argv[i] = NULL, argv);
 }
