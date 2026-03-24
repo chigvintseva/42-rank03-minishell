bd2c441b8c390adad55829a3362ab0c2e1a6b31d
chigvintseva <al.chigvintseva@gmail.com>
2026-03-24 17:25:04 +0100

    sasha's argv version, builtins

--- src/parser/cmd_argv.c
+++ src/parser/cmd_argv.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_argv.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: aleksandra <aleksandra@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 13:06:25 by achigvin          #+#    #+#             */
-/*   Updated: 2026/03/21 21:53:42 by achigvin         ###   ########.fr       */
+/*   Updated: 2026/03/22 15:07:21 by aleksandra       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,47 +27,34 @@ static int	redir_issue(t_token *cur, t_token *end)
 	}
 	return (0);
 }
-static int	fill_argv(char **argv, t_token *cur, t_token *end, int *redir_break)
-{
-	int	i;
-
-	i = 0;
-	while (cur != end)
-	{
-		if (is_word_token(cur->type))
-		{
-			i = add_word(argv, i, cur);
-			if (i < 0)
-				return (-1);
-		}
-		else if (is_redir_token(cur->type))
-		{
-			if (redir_issue(cur, end))
-				return (-1);
-			*redir_break = 1;
-			break ;
-		}
-		cur = cur->next;
-	}
-	return (i);
-}
 
 char	**extract_argv(t_token *cur, t_token *end, int argc)
 {
 	char	**argv;
 	int		i;
-	int		redir_break;
 
 	argv = malloc(sizeof(char *) * (argc + 1));
 	if (!argv)
 		return (NULL);
-	redir_break = 0;
-	i = fill_argv(argv, cur, end, &redir_break);
-	if (i < 0)
-		return (free_part_argv(argv, i), NULL);
-	if (!redir_break)
-		i = add_word(argv, i, end);
-	if (i < 0)
-		return (free_part_argv(argv, i), NULL);
+	i = 0;
+	while (cur)
+	{
+		if (is_word_token(cur->type))
+		{
+			argv[i] = ft_strdup(cur->value);
+			if (!argv[i])
+				return (free_part_argv(argv, i), NULL);
+			i++;
+		}
+		else if (is_redir_token(cur->type))
+		{
+			if (redir_issue(cur, end))
+				return (free_part_argv(argv, i), NULL);
+			break ;
+		}
+		if (cur == end)
+			break ;
+		cur = cur->next;
+	}
 	return (argv[i] = NULL, argv);
 }
