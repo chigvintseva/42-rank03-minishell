ca546b531d13e95ae8cd7bf966b8de2e92073ad5
chigvintseva <al.chigvintseva@gmail.com>
2026-03-24 17:48:49 +0100

    new line after all args only

--- src/runner/builtins/echo.c
+++ src/runner/builtins/echo.c
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aleksandra <aleksandra@student.42.fr>      +#+  +:+       +#+        */
+/*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 14:55:11 by aleksandra        #+#    #+#             */
-/*   Updated: 2026/03/24 15:09:27 by aleksandra       ###   ########.fr       */
+/*   Updated: 2026/03/24 17:47:18 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-int	builtin_echo(char **argv) // prints arg to stdout 
+int	builtin_echo(char **argv)
 {
 	size_t	i;
 	if (!argv[1])
-		return (write (1, "\n", 1), EXIT_SUCCESS); // or 1
+		return (write (1, "\n", 1), EXIT_SUCCESS);
 	if (!ft_strcmp(argv[1], "-n"))
 	{
 		if (!argv[2])
-			return (EXIT_SUCCESS); // or 1
+			return (EXIT_SUCCESS);
 		i = 2;
 		while (argv[i])
 		{
@@ -33,9 +33,12 @@ int	builtin_echo(char **argv) // prints arg to stdout
 		i = 1;
 		while (argv[i])
 		{
-			printf("%s\n", argv[i]);
+			printf("%s", argv[i]);
 			i++;
+			if (argv[i])
+				printf(" ");
 		}
 	}
+	printf("\n");
 	return (EXIT_SUCCESS);
 }
